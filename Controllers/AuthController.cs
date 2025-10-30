255ddb2f0087b582962ac568a559a5752901dded
Izie Zahba <dushara.fahzad62@gmail.com>
2025-10-30 13:04:49 +0800

    topic: Create Auth Controller and User Dto

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using JwtAuthLearn.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthLearn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+
+        }
+    }
+}
